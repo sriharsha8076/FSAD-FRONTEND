8890eec55a2e8b25a7d9910faef12b72191cc20e
sriharsha8076 <2400031017@kluniversity.in>
2026-04-03 08:34:55 +0530

    fix(spring): allow CORS from all origins for Vercel

--- src/main/java/com/fsad/backend/security/SecurityConfig.java
+++ src/main/java/com/fsad/backend/security/SecurityConfig.java
@@ -47,11 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:3000",
-                        "http://localhost:5177")); // Add your
-        // frontend
-        // URLs here
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
